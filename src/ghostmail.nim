481239782f4e67e247af7d775c4f2c4919ce18a0
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2025-11-09 16:58:35 +0300

    Create ghostmail.nim

---
+++ src/ghostmail.nim
@@ -0,0 +1,44 @@
+import asyncdispatch, httpclient, json, strutils, htmlparser, xmltree
+
+const api = "https://api.ghost-mail.io"
+var headers = newHttpHeaders({
+    "Connection": "keep-alive",
+    "user-agent":"Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.3",
+    "Host": "api.ghost-mail.io",
+    "Content-Type": "application/json",
+    "accept": "application/json"
+  })
+var token=""
+
+proc get_domains*(): Future[JsonNode] {.async.} =
+  let client = newAsyncHttpClient()
+  try:
+    client.headers = headers
+    let response = await client.get(api & "/v1/domains")
+    let body = await response.body
+    result = parseJson(body)
+  finally:
+    client.close()
+
+proc get_emails*(): Future[JsonNode] {.async.} =
+  let client = newAsyncHttpClient()
+  try:
+    client.headers = headers
+    let response = await client.post(api & "/v1/mailbox/random",body="{}")
+    let body = await response.body
+    let json = parseJson(body)
+    token = json["token"].getStr()
+    result = json
+  finally:
+    client.close()
+
+proc get_messages*(email_token:string=""): Future[JsonNode] {.async.} =
+  if email_token!="":token = email_token
+  let client = newAsyncHttpClient()
+  try:
+    client.headers = headers
+    let response = await client.get(api & "/v2/emails?token=" & token)
+    let body = await response.body
+    result = parseJson(body)
+  finally:
+    client.close()
